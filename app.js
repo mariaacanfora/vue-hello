8ca069d15321a87cd73144ac39518cab79c2e733
mariaacanfora <ma.maria.acanfora@gmail.com>
2021-10-29 09:52:06 +0200

    Used method "today" to print current day in html

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const vueApp = new Vue({
     el: '#root',
     data: {
         myName: "Maria",
-        today: new Date().toLocaleDateString().substr(0, 10).replaceAll("/", "-"),
+        //today: new Date().toLocaleDateString().substr(0, 10).replaceAll("/", "-"),
         time: new Date().toLocaleTimeString()
     },
+
+    methods:{
+        today(){
+           const oggi = new Date().toLocaleDateString().substr(0, 10).replaceAll("/", "-")
+           return oggi
+        }
+
+        
+    }
 });
